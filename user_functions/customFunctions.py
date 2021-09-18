8997542d9a9e06c54b3e280143e2048e4a23c6b0
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-09-18 04:11:25 +0200

    formatting

--- user_functions/customFunctions.py
+++ user_functions/customFunctions.py
@@ -5,18 +5,21 @@ def runCustomFunction(func):
     variables = func[func.find("(")+1:func.find(")")]
     variables = variables.replace(" ", "")
     variables = variables.split(",")
+    
     for i, variable in enumerate(variables):
         variables[i] = bulbVariables.getVariable(str(variable).lower(), True, True)
+        
     addedVars = False
     func = func.split("(")[0]
     func += "("
     for x in variables:
         func += str(x).lower() + ", "
         addedVars = True
+        
     if addedVars:
         func = func[:-2]
     func += ")"
-    print(func)
+    
     return(eval(func)) #Returns the value which will be assigned to the default variable "returned"
 
 ######ADD YOUR FUNCTIONS BELOW######
