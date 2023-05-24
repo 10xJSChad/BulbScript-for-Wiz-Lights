f271cdcc03e82b44d5928b04fd1dbff2f4ee1f53
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2023-05-24 17:58:35 +0200

    slight style change, hopefully improved readability

--- interpreter.py
+++ interpreter.py
@@ -1,6 +1,5 @@
 import asyncio
 import sys
-import time
 import config
 import random
 from bulbScript_functions import bulbSet, bulbVariables, bulbFunctions
@@ -11,10 +10,6 @@ funcCode = []
 recordCode = False
 skip = 0
 
-#Strips leading spaces
-def stripSpaces(cmd):
-    return(cmd.lstrip())
-
 async def initialize(script):
     await bulbSet.initialize(config.broadcastSpace)
     await parseCode(script)
@@ -22,56 +17,84 @@ async def initialize(script):
 async def parseCommand(cmd):
     global funcName, funcCode, recordCode, skip
     
-    cmd = stripSpaces(cmd) #Strips leading spaces
-    cmdUnchanged = cmd #Stores the command before splitting & setting it to lowercase
-    #To be honest, I'm not entirely sure why I felt the need for this.
+    cmd = cmd.lstrip()
+    cmdUnchanged = cmd
     
-    cmd = cmd.split("//")[0] #Strips everything following "//" in a line, so "//" may be used to write comments in BulbScript scripts.
-    cmd = cmd.lower() #Converts the line to lowercase to prepare it for further parsing
-    splitCmd = cmd.split(" ") #Splits the string by space to prepare for further parsing
+    cmd = cmd.split("//")[0] # removes the comment if there is one
+    cmd = cmd.lower() # lowercase for simpler parsing
+    splitCmd = cmd.split(" ")
     
-    if splitCmd[0] == "if" and skip > 0: #Increments the skip variable, this is needed for nested conditionals to work
+
+    # keeps track of the current "if statement" depth
+    # this is necessary for nested conditionals to work properly
+    # the old solution for if statements was to just skip to the next 'end if'
+    # but that would often result in jumping to the end of a child condition
+    # despite the parent condition being evaluated to false
+
+    if splitCmd[0] == "if" and skip > 0:
         skip += 1 
-    if splitCmd[0] == "end" and splitCmd[1] == "if" and skip > 0: #Decrements it on an "end if" statement
+    if splitCmd[0] == "end" and splitCmd[1] == "if" and skip > 0:
         skip -= 1 
         
     if skip > 0: return
 
-    if recordCode: funcCode.append(cmd) #Appends lines of BulbScript code to the funcCode list if a function has been declared    
+
+    # if a function is declared, capture all the code until the next 'end func'
+    # nested function definitions are not supported.
+    if recordCode: funcCode.append(cmd)
+
     if splitCmd[0] == "func":
-        funcName = splitCmd[1] #Sets the function name to the first word after a space
-        recordCode = True #A function has been declared, so recordCode is now true
-        #all lines of code until an 'end func' statement will be appended into the funcCode list.
+        funcName = splitCmd[1]
+        recordCode = True
+
     if splitCmd[0] == "end" and splitCmd[1] == "func":
-        funcCode.pop(-1) #Removes the last entry in the funcCode list. The last entry will always be 'end func', and we don't want that
-        bulbFunctions.addFunction(funcName, funcCode) #Adds the function to the function list in bulbFunctions
+        funcCode.pop(-1) # remove the "end func" from the function code
+
+        # adds a function entry with the name and captured code
+        bulbFunctions.addFunction(funcName, funcCode)
+
         funcCode = [] 
         recordCode = False
-    if splitCmd[0] == "call": #Checks for a 'call' command, which is used to call a function
-        bulbFunction = bulbFunctions.getFunction(splitCmd[1])[1] #Gets the code of the function called. 
-        #bulbFunctions.getFunction takes a function name and returns ["Function Name", ["Function code]]
-        #The code above will store only the code in bulbFunction
-        if len(splitCmd) == 3 and splitCmd[2] != "": #Checks if there's a third argument, which would be used to call a function X amount of times
-            for i in range(0, int(splitCmd[2])): #Calls the function however many time specified. ex. "call testFunc 10" <- calls testFunc 10 times
-                await parseCode(bulbFunction, True) #Sends the function code to be parsed and ran, with runOnce set to True to prevent it from looping infinitely
+
+    # function calls
+    # checks for a function call and executes the function if once is present
+    # if the function call does not specify how many times to execute the function, 
+    # then the function is executed only once.
+
+    if splitCmd[0] == "call":
+        bulbFunction = bulbFunctions.getFunction(splitCmd[1])[1]
+        if len(splitCmd) == 3 and splitCmd[2] != "":
+            for _ in range(int(splitCmd[2])):
+                await parseCode(bulbFunction, True)
             return        
         await parseCode(bulbFunction, True)
         
     if not recordCode:
-        #Command parsing
-        
+        # this should have been a dictionary
+
         if splitCmd[0] == "setrgb":
-            await bulbSet.setRgb(bulbVariables.getAllVariablesFromRgbInput(cmd), bulbVariables.getVariable(splitCmd[1], True, True));
+            await bulbSet.setRgb(bulbVariables.getAllVariablesFromRgbInput(cmd), 
+                                 bulbVariables.getVariable(splitCmd[1], True, True))
+            
         if splitCmd[0] == "setbrightness":
-            await bulbSet.setBrightness(bulbVariables.getVariable(splitCmd[1], True, True), bulbVariables.getVariable(splitCmd[2], True, True));
+            await bulbSet.setBrightness(bulbVariables.getVariable(splitCmd[1], True, True), 
+                                        bulbVariables.getVariable(splitCmd[2], True, True))
+            
         if splitCmd[0] == "setscene":
-            await bulbSet.setScene(bulbVariables.getVariable(splitCmd[1], True, True), bulbVariables.getVariable(splitCmd[2], True, True));
+            await bulbSet.setScene(bulbVariables.getVariable(splitCmd[1], True, True), 
+                                   bulbVariables.getVariable(splitCmd[2], True, True))
+            
         if splitCmd[0] == "setwhite":
-            await bulbSet.setWhite(splitCmd[2], bulbVariables.getVariable(splitCmd[3], True, True), bulbVariables.getVariable(splitCmd[1], True, True));
+            await bulbSet.setWhite(splitCmd[2], 
+                                   bulbVariables.getVariable(splitCmd[3], True, True), 
+                                   bulbVariables.getVariable(splitCmd[1], True, True))
+        
         if splitCmd[0] == "setoff":
-            await bulbSet.setOff(bulbVariables.getVariable(splitCmd[1], True, True));
+            await bulbSet.setOff(bulbVariables.getVariable(splitCmd[1], True, True))
+
         if splitCmd[0] == "slp":
-            await asyncio.sleep(float(splitCmd[1]));
+            await asyncio.sleep(float(splitCmd[1]))
+
         if splitCmd[0] == "var":
             varName = splitCmd[1]
             if len(splitCmd) > 2:
@@ -79,21 +102,29 @@ async def parseCommand(cmd):
                 varValue = varValue.replace(" ", "")
             else: varValue = ""
             bulbVariables.addVariable(varName, varValue)
+
         if splitCmd[0] == "mov" or splitCmd[0] == "set":
             bulbVariables.setVariable(splitCmd[1], splitCmd[2])
+
         if splitCmd[0] == "add":
             bulbVariables.addToVariable(splitCmd[1], splitCmd[2])
+
         if splitCmd[0] == "sub":
             bulbVariables.subFromVariable(splitCmd[1], splitCmd[2])
+
         if splitCmd[0] == "exec":
             bulbVariables.setVariable("returned", customFunctions.runCustomFunction(cmdUnchanged[5:]))
+
         if splitCmd[0] == "print":
             bulbVariables.formatAndPrint(cmd[6:])
+
         if splitCmd[0] == "rnd":
             rnd = random.randint(int(splitCmd[2]), int(splitCmd[3]))
             bulbVariables.setVariable(splitCmd[1], rnd)
+
         if splitCmd[0] == "stop":
             return "stop"
+        
         if splitCmd[0] == "if":
             if not (bulbVariables.compareVariable(splitCmd[1], splitCmd[3], splitCmd[2])): skip += 1 
     
